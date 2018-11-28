1fce79ad06e953acf9ae1216ab5b8cfc168954b7
mark <marcmumo@gmail.com>
2018-11-28 10:56:06 +0300

    [chore #162191421] added GetIncident route

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 
-from app.api.v1.views import PostIncidents
+from app.api.v1.views import PostIncidents, GetIncidents
 
 
 def create_app():
@@ -9,5 +9,6 @@ def create_app():
     app = Flask(__name__)
     user = Api(app)
     user.add_resource(PostIncidents, "/api/v1/incident")
+    user.add_resource(GetIncidents, "/api/v1/incident")
 
     return app
