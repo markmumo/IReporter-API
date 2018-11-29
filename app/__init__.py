d537d1089428af47f653b56845c009714afcbfaa
mark <marcmumo@gmail.com>
2018-11-29 21:23:43 +0300

    [chore #162191570] modified routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_restful import Api
+from instance.config import app_config
+from app.api.v1.views import PostIncidents, Incidents, Get_incident_by_id
 
-from app.api.v1.views import PostIncidents, Incidents, Get_incident_by_id, Get_specific_incident
 
+def create_app(config_mode):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_mode])
+    app.config.from_pyfile('config.py', silent=True)
 
-def create_app():
-
-    app = Flask(__name__)
     api = Api(app)
 
     """(POST) create incidents"""
@@ -18,10 +20,7 @@ def create_app():
     """GET incident by id"""
     api.add_resource(Get_incident_by_id, "/api/v1/incident/<int:id>")
 
-    """GET specific incidents """
-    api.add_resource(Get_specific_incident, "/api/v1/incident/<int:id>")
-
-    """"DELETE incident by id"""
-    api.add_resource(Get_incident_by_id, "/api/v1/incident/<int:id>")
+    # """GET specific incidents """
+    # api.add_resource(Get_specific_incident, "/api/v1/incident/<int:id>")
 
     return app
