55d3a40e6c5b47a76e3e92573a87cdd18735bffa
mark <marcmumo@gmail.com>
2018-11-29 21:30:03 +0300

    [chore #162191570] modified methods

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -18,6 +18,7 @@ class PostIncidents(Resource):
     parser.add_argument("comment", type=str, required=True)
 
     def post(self):
+
         data = PostIncidents.parser.parse_args()
 
         created_by = data["created_by"]
@@ -31,6 +32,7 @@ class PostIncidents(Resource):
         validating = Validate()
 
         if not validating.is_string(created_by):
+
             return {"message": "must be a string"}, 400
 
         if not validating.is_string(Type):
@@ -39,8 +41,6 @@ class PostIncidents(Resource):
         incident = Incident(created_by, Type, location,
                             status, comment, image, video)
 
-        print(f"{incident.Type}")
-
         incidents.append(incident)
 
         return {"incident": "created successfully"}, 201
@@ -48,64 +48,47 @@ class PostIncidents(Resource):
 
 class Incidents(Resource):
 
+    """ GET all incident """
+
     def get(self):
-        return {"Incidents": [Incident.serializer() for Incident in incidents]}
 
-
-class Get_specific_incident(Resource):
-
-    parsing = reqparse.RequestParser()
-    parsing.add_argument("created_by", type=str,
-                         required=True, help="This field is required")
-    parsing.add_argument("Type", type=str, required=True,
-                         help="This field is required")
-    parsing.add_argument("location", type=str, required=True,
-                         help="This field is required")
-    parsing.add_argument("status", type=str, required=True,
-                         help="This field is required")
-    parsing.add_argument("image", type=str, required=True,
-                         help="This field is required")
-    parsing.add_argument("video", type=str, required=True,
-                         help="This field is required")
-    parsing.add_argument("comment", type=str, required=True,
-                         help="This field is required")
-
-    """ GET specific incident """
-
-    def get(self, id):
-        incident = Incident().get_incident_by_id(id)
-        if not incident:
-            return {"Message": "incident does not exit"}, 404
-        else:
-            return {"Incident": incident.serializer()}, 200
-
-    """ GET incident by id """
+        return {"Incidents": [Incident.serializer() for Incident in incidents]}, 200
 
 
 class Get_incident_by_id(Resource):
 
+    """ GET incident by id """
+
     def get(self, id):
+
         incident = Incident().get_incident_by_id(id)
+
         if not incident:
+
             return {"Message": "incident does not exit"}, 404
+
         else:
+
             return {"Incident": incident.serializer()}, 200
 
-    """" DELETE specific incident """
-
     def delete(self, id):
+        """" DELETE specific incident """
+
         delete_incident = Incident().get_incident_by_id(id)
         if not delete_incident:
+
             return {"message": "incident does not exist"}, 404
+
         else:
+
             incidents.remove(delete_incident)
+
             return {"Message": "Incident deleted successfully"}, 200
 
-    """ (PATCH) update specific incident """
-
     def patch(self, id):
+        """ (PATCH) update specific incident """
 
-        data = Get_specific_incident.parsing.parse_args()
+        data = PostIncidents.parser.parse_args()
 
         created_by = data['created_by']
         Type = data['Type']
@@ -118,8 +101,11 @@ class Get_incident_by_id(Resource):
         specific_incident = Incident().get_incident_by_id(id)
 
         if not specific_incident:
+
             return {"message": "This incident does not exist"}, 404
+
         else:
+
             specific_incident.created_by = created_by
             specific_incident.Type = Type
             specific_incident.location = location
