9fa67acfc52ba80976791bc74bd124de15ed3846
mark <marcmumo@gmail.com>
2018-11-29 21:26:54 +0300

    [chore #162191570] added  env variables

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
+import os
 from app import create_app
 
-app = create_app()
+app = create_app(os.getenv("APP_SETTINGS") or "development")
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
