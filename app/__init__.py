b383179234955644eed8db6c86be40f6441abc31
mark <marcmumo@gmail.com>
2018-12-05 20:25:21 +0300

    Updated config file to allow heroku deployment

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_restful import Api
-from instance.config import app_config
 from app.api.v1.url import url
+from instance.config import config
 
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
+    app.config.from_object(config[config_name])
 
     from app.api.v1 import api_bp as api_blueprint
     api = Api(api_blueprint)
