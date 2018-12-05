975fbed08074c243b62b2f64c1a09ac3954326fe
mark <marcmumo@gmail.com>
2018-12-05 20:29:30 +0300

    Updated .gitignore file to allow heroku deployment

---
+++ instance/config.py
@@ -0,0 +1,29 @@
+
+class Config:
+    """Parent configuration class."""
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    """Configurations for Development."""
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    """Configurations for Testing, with a separate test database."""
+    TESTING = True
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    """Configurations for Production."""
+    DEBUG = False
+    TESTING = False
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
